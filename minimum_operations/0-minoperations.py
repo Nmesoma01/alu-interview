f08c4151b5291b70ad7b53699350ae41b134dc70
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-02-02 16:52:37 -0800

    Minoperation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,16 +6,11 @@ def minOperations(n):
     # Return 0 if n is less than or equal to 0
     if n <= 0:
         return 0
-    
     operations = 0 # Initialize the number of operations to 0
-    
-    # Keep dividing n by 2 until it becomes 1
     while n != 1:
         if n % 2 == 0: # Check if n is divisible by 2
             n = n // 2 # Divide n by 2
             operations += 1 # Increment the number of operations by 1
         else:
-            return 0 # If n is not divisible by 2, it's impossible to achieve n H's
-    
-    # Return the total number of operations
+            return 0 
     return operations
