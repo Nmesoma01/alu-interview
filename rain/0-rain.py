bb938b4d2cd812f9ba829ee934e1a0af4aa1b4bb
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-02-20 14:45:44 -0800

    changes

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 """
-Rain
+calculate how many square units of water will be retained after it rains
 """
-
-
 def rain(walls):
     """
-    Return total amount of water retained.
+    Provide the cumulative volume of water that has been held or trapped.
     """
     if not walls:
         return 0
 
     water = 0
     for i in range(len(walls)):
+
         if walls[i] != max(walls):
-            max_lef = 0
+            max_left = 0
             max_right = 0
             for j in range(0, i):
-                if max_lef < walls[j]:
-                    max_lef = walls[j]
+                if max_left < walls[j]:
+                    max_left = walls[j]
+
             for k in range(i, len(walls)):
                 if max_right < walls[k]:
                     max_right = walls[k]
-            if max_lef != 0 and max_right != 0:
-                if max_lef < max_right and max_lef > walls[i]:
-                    water += max_lef - walls[i]
-                elif max_right <= max_lef and max_right > walls[i]:
+            if max_left != 0 and max_right != 0:
+                if max_left < max_right and max_left > walls[i]:
+                    water += max_left - walls[i]
+                elif max_right <= max_left and max_right > walls[i]:
                     water += max_right - walls[i]
+    
     return water
