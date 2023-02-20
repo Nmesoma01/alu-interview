123e429ca444ca6de791633ea094bf266b1e8fe6
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-02-20 14:33:29 -0800

    changes

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""
+Rain
+"""
 
 def rain(walls):
-    if not walls:
-        return 0
-    left_max = [0] * len(walls)
-    right_max = [0] * len(walls)
-    left_max[0] = walls[0]
-    right_max[-1] = walls[-1]
-    for i in range(1, len(walls)):
-        left_max[i] = max(left_max[i-1], walls[i])
-    for i in range(len(walls)-2, -1, -1):
-        right_max[i] = max(right_max[i+1], walls[i])
-    total = 0
-    for i in range(len(walls)):
-        total += min(left_max[i], right_max[i]) - walls[i]
-    return max(total, 0)
+"""
+Return total amount of rainwater retained.
+"""
+if not walls:
+return 0
+
+rainwater = 0
+for i in range(len(walls)):
+    if walls[i] == max(walls):
+        continue
+
+    max_lef = max(walls[:i])
+    max_right = max(walls[i:])
+
+    if max_lef > walls[i] and max_right > walls[i]:
+        if max_lef < max_right:
+            rainwater += max_lef - walls[i]
+        else:
+            rainwater += max_right - walls[i]
+return rainwater
