8c0aaa237269ca0049ec48556ceef34f2a0ac3d7
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-03-14 16:08:36 -0700

    my commit

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""
-pascal's triangle
-Generates Pascal's Triangle up to a given index.
+"""the 0-pascal_triangle module
+defines a function `pascal_triangle`
 """
 
 
 def pascal_triangle(n):
-    """get pascal triangle to a certain index"""
-def generate_pascal_triangle(n):
-    """
-    Returns a list of lists representing the Pascal's Triangle up to the nth row.
-    """
-    if n <= 0:
-        return []
-    result = [[1], [1, 1]]
-    triangle = [[1], [1, 1]]
-    for i in range(2, n):
-        list_above = [0] + result[i - 1] + [0]
-        prev_row = [0] + triangle[i - 1] + [0]
-        row = []
-        for j in range(0, len(list_above) - 1):
-            row.append(list_above[j] + list_above[j + 1])
-        result.append(row)
-    return result[:n]
-        for j in range(0, len(prev_row) - 1):
-            row.append(prev_row[j] + prev_row[j + 1])
-        triangle.append(row)
-    return triangle[:n]
+    """returns a pascal tirangle of size `n`"""
+    if n > 0:
+        pasc_tri = []
+        for i in range(n):
+            row = []
+            if len(pasc_tri) > 0 and len(pasc_tri[-1]) > 0:
+                row.append(1)
+                for x, y in zip(pasc_tri[-1], pasc_tri[-1][1:]):
+                    row.append(x + y)
+            row.append(1)
+            pasc_tri.append(row)
+
+        return pasc_tri
+
+    return []
