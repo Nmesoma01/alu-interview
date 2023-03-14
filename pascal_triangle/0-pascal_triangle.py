506cf8ef37bebca0210b9bb561cce2b029a176c1
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-03-14 15:54:04 -0700

    my commit

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""
+pascal's triangle
+Generates Pascal's Triangle up to a given index.
+"""
 
 
-"""
-    0-pascal_triangle.py
-"""
 def pascal_triangle(n):
-     """
-     Creates a list of lists of integers representing
-         the Pascal's triangle of a given integer.
-         """
-            triangle = []
-                if type(n) is not int or n <= 0:
-                        return triangle
-                    for i in range(n):
-                                line = []
-                                        for j in range(i + 1):
-                                                        if j == 0 or j == i:
-                                                                            line.append(1)
-                                                                                        elif i > 0 and j > 0:
-                                                                                                            line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-                                                                                                                    triangle.append(line)
-                                                                                                                        return triangle
-    '''Create a function def pascal_triangle(n):
-    that returns a list of lists of integers representing the Pascal’s triangle of n:
-    '''
-    list1 = []
-    if type(n) is not int or n <= 0:
-        return triangle
-    for i in range(n):
-        temp_list = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                temp_list.append(1)
-            elif i > 0 and j > 0:
-                temp_list.append(list1[i - 1][j - 1] + list1[i - 1][j])
-        list1.append(temp_list)
-    return list1
+    """get pascal triangle to a certain index"""
+def generate_pascal_triangle(n):
+    """
+    Returns a list of lists representing the Pascal's Triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+    result = [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        list_above = [0] + result[i - 1] + [0]
+        prev_row = [0] + triangle[i - 1] + [0]
+        row = []
+        for j in range(0, len(list_above) - 1):
+            row.append(list_above[j] + list_above[j + 1])
+        result.append(row)
+    return result[:n]
+        for j in range(0, len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
+        triangle.append(row)
+    return triangle[:n]
