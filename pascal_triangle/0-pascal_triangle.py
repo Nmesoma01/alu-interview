3dff0b20d91d55efe1a9d44746510b2cabf40087
Nmesoma01 <n.jeff-nwankwo@alustudent.com>
2023-03-14 15:50:26 -0700

    my commit

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,40 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
 
- def pascal_triangle(n):
-    # Check if n is less than or equal to 0
-    if n <= 0:
-        # Return an empty list if n is less than or equal to 0
-        return []
 
-    # Initialize the triangle with the first row, which is just [1]
-    triangle = [[1]]
-
-    # Iterate from 1 to n-1 to compute each subsequent row
-    for i in range(1, n):
-        # Initialize a new list for the current row, with a 1 at the beginning
-        row = [1]
-
-        # Iterate from 1 to i-1 to compute the values in the middle of the row
-        for j in range(1, i):
-            # Compute the value in the middle of the row by summing the corresponding values from the previous row
-            # The value at position (i-1, j-1) corresponds to the left neighbor, and the value at position (i-1, j) corresponds to the right neighbor
-            middle_value = triangle[i-1][j-1] + triangle[i-1][j]
-            # Append the middle value to the current row
-            row.append(middle_value)
-
-        # Append another 1 at the end of the row
-        row.append(1)
-
-        # Append the completed row to the triangle list
-        triangle.append(row)
-
-    # Return the completed triangle
-    return triangle
+"""
+    0-pascal_triangle.py
+"""
+def pascal_triangle(n):
+     """
+     Creates a list of lists of integers representing
+         the Pascal's triangle of a given integer.
+         """
+            triangle = []
+                if type(n) is not int or n <= 0:
+                        return triangle
+                    for i in range(n):
+                                line = []
+                                        for j in range(i + 1):
+                                                        if j == 0 or j == i:
+                                                                            line.append(1)
+                                                                                        elif i > 0 and j > 0:
+                                                                                                            line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+                                                                                                                    triangle.append(line)
+                                                                                                                        return triangle
+    '''Create a function def pascal_triangle(n):
+    that returns a list of lists of integers representing the Pascal’s triangle of n:
+    '''
+    list1 = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            elif i > 0 and j > 0:
+                temp_list.append(list1[i - 1][j - 1] + list1[i - 1][j])
+        list1.append(temp_list)
+    return list1
